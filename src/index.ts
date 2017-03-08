ba01d2ff921b48173446d3c36a75f7b885e3de49
tomschreck <tom_schreck@solutiaconsulting.com>
2017-03-08 17:19:16 -0600

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 /**
  * This barrel file provides the exports for shared resources
  */
+export * from './components/index';
 export * from './models/index';
-export * from './cdf-media.module';
 export * from './config';
+export * from './cdf-media.module';
